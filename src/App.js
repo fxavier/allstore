308556014173996e6d55f599baeb90c4202a6a9d
Xavier Francisco <xavierfrancisco353@gmail.com>
2020-03-30 16:46:37 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return <h1>Hello from All Store</h1>;
+  }
+}
+
+export default App;
